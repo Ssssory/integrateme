fbf0c7e800fbc429ab4474e7ce14805255cfd94f
Trubiv <itrubchaninov@list.ru>
2020-01-12 17:16:01 +0000

    add api term

---
+++ app/Http/Controllers/TermController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Term;
+
+// use Response
+
+class TermController extends Controller
+{
+    
+    
+    public function create(Request $request){
+
+        $phone = $request->input('phone', null);
+        if (is_null($phone)) {
+            return response()->json(['result'=>'error','text'=>'empty phone']);
+        }
+        $term = Term::where('phone', $phone)->first();
+        if ($term) {
+            return response()->json(['result'=>'error','text'=>'phone already in system']);
+        }
+        $newTerm = new Term;
+        $newTerm->phone = $phone;
+        $newTerm->save();
+
+        return response()->json(['result'=>'sucess']);
+    }
+}
