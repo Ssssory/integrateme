e1fcda57a0e5e869bb6a3d23f2067dda7b17f083
Trubiv <itrubchaninov@list.ru>
2020-01-13 08:27:35 +0000

    add CORS middleware

--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::group(['middleware' => 'cors'], function () {
+        Route::post('/terms', 'TermController@create');
+});
 
 
-Route::post('/terms', 'TermController@create');
 
 
